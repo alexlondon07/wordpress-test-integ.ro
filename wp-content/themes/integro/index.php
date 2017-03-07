455839291a571e38578f02e100281a64b36e6055
Alexander Londoño Espejo <alexlondon07@gmail.com>
2017-03-07 14:44:18 -0500

    Agregando The_Loop para mostrar nuestros posts

--- wp-content/themes/integro/index.php
+++ wp-content/themes/integro/index.php
@@ -4,14 +4,31 @@
     <div class="grid max-width">
             <div class="block grid--item-9">
                  <div class="block__title">
-                    Bloque principal
+                    Últimos trabajos
                 </div>
-                 <div class="block__body">
+                 <div class="block__body grid">
                     <p>Integ.ro una organización especializada en diseñar, estructurar y desarrollar soluciones integrales de tecnología web.</p>
+                    <?php 
+                        if( have_posts()){
+                            while(have_posts()){
+                                the_post();
+                                ?>
+                                <article class="block grid--item-4">
+                                    <h2 class="block__title"><?php the_title(); ?><h2>
+                                    <div class="block__body">
+                                        <!-- Informacion de cada post-->
+                                        <p><?php the_excerpt(); ?></p>
+
+                                        <!-- tags de nuetros post-->
+                                        <footer><small><?php the_tags(); ?></small></footer>
+                                    </div>
+                                </article>
+                                <?php
+                            }
+                        }
+                    ?>
                 </div>
             </div>
             <?php  get_sidebar(); ?>
         </div>
         <?php  get_footer(); ?>
-</body>
-</html>
